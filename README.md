6571395d631c3f1d22b2692b5280c4e0f008ff44
Nikolaj Kappler <nikolaj.kappler@sap.com>
2021-02-12 17:03:24 +0100

    created build script for docs

---
+++ README.md
@@ -0,0 +1,20 @@
+![./docs/lcov-report/index.html](https://img.shields.io/badge/coverage-100%25-brightgreen)
+# zuteil
+
+A highly configurable dispatcher for async jobs, written in TypeScript
+
+## What zuteil is:
+
+- 
+- 
+
+## What zuteil is **not**:
+
+-
+-
+
+```typescript
+// Syntax Highlighting Test
+
+const f: (x: string) => number = (arg: string) => 4;
+```
---
+++ docs/compileMarkdown.js
@@ -0,0 +1,21 @@
+var hljs = require('highlight.js'),
+    md = require('markdown-it')({
+        highlight: function (str, lang) {
+            if (lang && hljs.getLanguage(lang)) {
+                try {
+                    return '<pre class="hljs"><code>' +
+                        hljs.highlight(lang, str, true).value +
+                        '</code></pre>';
+                } catch (__) { }
+            }
+
+            return ''; // use external default escaping
+        }
+    }),
+    fs = require('fs');
+
+var source = fs.readFileSync("README.md").toString();
+var template = fs.readFileSync("./docs/template.html").toString();
+var result = template.replace("{{{CONTENT}}}", md.render(source));
+
+fs.writeFileSync("./docs/index.html", result);
